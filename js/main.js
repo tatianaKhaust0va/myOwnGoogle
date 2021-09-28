97de837f53c37e211aab1413736874ea3204aa27
tatos <tatossa.hau994@gmail.com>
2021-09-28 10:04:29 +0300

    Deleted JS, modified styles & index.Html

--- js/main.js
+++
@@ -1,8 +0,0 @@
-let form = document.querySelectorAll("form");
-
-form.addEventListener("submit", function (e) {
- e.preventDefault();
- let search = form.querySelectorAll('#search');
- search.value = "search:css-search.com " + search.value;
- form.submit();
-});
